b5fe95588dd517b5d7bb3a654b2775c4c8d514a8
AiziChen <1098679804@qq.com>
2024-08-02 09:15:32 +0800

    response_header: also remove the address operator `&`

--- src/tools.zig
+++ src/tools.zig
@@ -14,9 +14,9 @@ pub fn is_http_header(header: []const u8) bool {
 pub fn response_header(header: []const u8) []const u8 {
     var buffer: [4096]u8 = undefined;
     const lowercase_header = std.ascii.lowerString(&buffer, header);
-    if (std.mem.containsAtLeast(u8, &lowercase_header, 1, "websocket")) {
+    if (std.mem.containsAtLeast(u8, lowercase_header, 1, "websocket")) {
         return "HTTP/1.1 101 Switching Protocols\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: CuteBi Network Tunnel, (%>w<%)\r\n\r\n";
-    } else if (std.mem.startsWith(u8, &lowercase_header, "connect")) {
+    } else if (std.mem.startsWith(u8, lowercase_header, "connect")) {
         return "HTTP/1.1 200 Connection established\r\nServer: CuteBi Network Tunnel, (%>w<%)\r\nConnection: keep-alive\r\n\r\n";
     } else {
         return "HTTP/1.1 200 OK\r\nTransfer-Encoding: chunked\r\nServer: CuteBi Network Tunnel, (%>w<%)\r\nConnection: keep-alive\r\n\r\n";
